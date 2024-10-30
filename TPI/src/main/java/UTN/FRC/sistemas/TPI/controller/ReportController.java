e9fb11049f8e64c8abc9af33cef5577b04baa7c9
leonls1 <leonlederhossturich@gmail.com>
2024-10-30 09:08:18 -0300

    Updated ReportController with endpoints:   -"incident-by-employee": return a list of incidents for the given employee id   -"test-by-vehicle": return a set of the test for the given vehicle id
     Updated Employee with method:
      -"getIncidents": return a list of incidents of the total test of the employee

--- TPI/src/main/java/UTN/FRC/sistemas/TPI/controller/ReportController.java
+++ TPI/src/main/java/UTN/FRC/sistemas/TPI/controller/ReportController.java
@@ -1,23 +1,30 @@
 package UTN.FRC.sistemas.TPI.controller;
 
+import UTN.FRC.sistemas.TPI.model.entities.Employee;
 import UTN.FRC.sistemas.TPI.model.entities.Test;
+import UTN.FRC.sistemas.TPI.service.EmployeeService;
 import UTN.FRC.sistemas.TPI.service.TestService;
+import UTN.FRC.sistemas.TPI.service.VehicleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RequestMapping("/api/v1/TPI/report/")
 @RestController
 @RequiredArgsConstructor
 public class ReportController {
     private final TestService testService;
+    private final EmployeeService employeeService;
+    private final VehicleService vehicleService;
     //f. Generar reportes de:
     //i. Incidentes (pruebas donde se excedieron los límites establecidos)
+        //pruebas donde se violaron las reglas del negocio(agregar campo a test de  incidente(boolean)
+        // si el test surgio algun inconveniente)
+
     @GetMapping("/incident")
     public ResponseEntity<?> getAllIncidents(){
         List<Test> incidents = testService.getAllIncidentTests();
@@ -28,12 +35,30 @@ public class ReportController {
 
     }
 
-        //pruebas donde se violaron las reglas del negocio(agregar campo a test de  incidente(boolean) si el test surgio algun inconveniente)
     //ii. Detalle de incidentes para un empleado
         //incidentes por empleado(combina el anterior ademas del empleado)
+    @GetMapping("/incident-by-employee/{employeeId}")
+    public ResponseEntity<?> getIncidentsByEmployee(@PathVariable Long employeeId){
+        List<Test> incidentByEmployee = employeeService.findById(employeeId).getIncidents();
+        if(!incidentByEmployee.isEmpty()){
+            return ResponseEntity.ok(incidentByEmployee);
+        }else{
+            return new ResponseEntity<>("There isn't incidents for the employee", HttpStatus.NO_CONTENT);
+        }
+
+    }
     //iii. Cantidad de kilómetros de prueba que recorrió un vehículo en un
     //período determinado.
         //km recorridos de un vehiculo en un periodo dado
     //iv. Detalle de pruebas realizadas para un vehículo
         //pruebas por vehiculo
+    @GetMapping("/test-by-vehicule/{vehicleId}")
+    public ResponseEntity<?> getTestByVehicleId(@PathVariable Long vehicleId ){
+        Set<Test> testsByVehicle = vehicleService.findById(vehicleId).getTests();
+        if(!testsByVehicle.isEmpty()){
+            return ResponseEntity.ok(testsByVehicle);
+        }else{
+            return new ResponseEntity<>("There isn't tests for the current vehicle", HttpStatus.NO_CONTENT);
+        }
+    }
 }
