376ab80496ebba2f696defbcbe1e9591ea39ec12
leonls1 <leonlederhossturich@gmail.com>
2024-11-07 20:29:51 -0300

    added basic structure for security

---
+++ Gateway/src/main/java/UTN/FRC/sistemas/Gateway/security/configuration/SecurityConfig.java
@@ -0,0 +1,9 @@
+package UTN.FRC.sistemas.Gateway.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+}
